1394f694739657681589d4b8751c98a9d23be665
AmbaRick <amba.rickards@hotmail.com>
2024-05-08 12:44:52 +0100

    Feat: adding ToDoItem as a entity class

---
+++ ToDo.Service/Entities/ToDoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Service.Entities
+{
+    /// <summary>
+    /// Class to define the properties of ToDoItem
+    /// </summary>
+    public class ToDoItem
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
